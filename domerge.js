3a839059b9e99877ec5ccf258012b0da7b685ce6
yapcheahshen <yapcheahshen@gmail.com>
2015-08-25 21:51:43 +0800

    merge vol 84 ok

--- domerge.js
+++ domerge.js
@@ -1,12 +1,23 @@
-var fs=require("fs");
-//var content=fs.readFileSync("before_xml/084/lj0352_001.xml","utf8");
-var filecontent="";
-var jsonfn=process.argv[2]||"jiangkangyur201504.json";
+var diffmode=true; 
+var jsonfn=process.argv[2]||"jiangkangyur201504.json";  //default json filename
 var json=require("./before_json/"+jsonfn);
-var lastfile="",pages={},currentfile="",currentpage="";
 
-var error=function(msg,start) {
-	throw msg+", start:"+start+" file:"+currentfile+" page:"+currentpage;
+//////////////////////////////////////////////////////////////////////
+
+var fs=require("fs");
+var mkdirp=require("./mkdirp");
+var path=require("path");
+//var content=fs.readFileSync("before_xml/084/lj0352_001.xml","utf8");
+var lastfile="",pages={},currentfile="",currentpage="";
+var unmerged=[],lasterror="",mergecount=0;
+
+var errormerge=function(item){
+	item.err=lasterror;
+	lasterror="";
+	unmerged.push(JSON.stringify(item));
+}
+var fatal=function(msg,offset) {
+	throw msg+", offset:"+offset+" file:"+currentfile+" page:"+currentpage;
 }
 var loadPage=function(filecontent) {
 	var page={},lastidx=0,lastid="_";
@@ -21,47 +32,84 @@ var loadPage=function(filecontent) {
 	page[lastid]=filecontent.substring(lastidx).trim();	
 	return page;
 }
-var fetchText=function(filecontent,start,len) {
-	var realoff=getOffsetOmitTag(filecontent,start);
-	if (realoff==-1) {
-		error("invalid offset",start);
-	}
-
-	return filecontent.substr(realoff,len);
+var fetchText=function(pagecontent,offset,len) {
+	var realoff=getOffsetOmitTag(pagecontent,offset);
+	if (typeof realoff !=="number") return null;
+	return pagecontent.substr(realoff,len);
 }
 
-var getOffsetOmitTag=function(pagecontent,start){
+var getOffsetOmitTag=function(pagecontent,offset){
 	var intag=false;
-	var remain=start;
+	var remain=offset;
 	for (var i=0;i<pagecontent.length;i++) {
 		var c=pagecontent[i];
 		if (c==='<') intag=true;
 
 		if (remain==0) {
 			if (intag) {
-				error("offset inside tag",start);
+				lasterror="offset inside tag";
+				return null;
 			} else {
 				return i;
 			}
 		}
 		if (!intag) remain--;
-
 		if (c==='>') intag=false;
 	}
-
-	return -1;
+	lasterror="invalid offset";
+	return null;
 }
-var domerge=function(item){	
+
+var merge=function(item) {
+	var page=pages[currentpage];
+	var realoff=getOffsetOmitTag(page,item.offset);
+	if (diffmode) {
+		page=page.substr(0,realoff)+"["+item.from+"|"+item.to+"]"+page.substr(realoff+item.from.length);;
+	} else {
+		page=page.substr(0,realoff)+item.to+page.substr(realoff+item.from.length);
+	}
+	pages[currentpage]=page;
+}
+
+var createnewfilecontent=function() {
+	var s="";
+	for (var i in pages) {
+		if (i!=="_") {
+			s+='<pb id="'+i+'"/>\n';
+		}
+		s+=pages[i];
+	}
+	return s;
+}
+
+var savemergefile=function() {
+	if (!lastfile)return;
+	var dir=path.dirname("merged_xml/"+lastfile);
+	mkdirp.sync(dir);	
+	fs.writeFileSync("merged_xml/"+lastfile,createnewfilecontent(),"utf8");
+}
+
+var trymerge=function(item){	
 	if (item.file!==lastfile) {
+		savemergefile();
 		currentfile=item.file;
-		filecontent=fs.readFileSync("before_xml/"+item.file,"utf8");
-		pages=loadPage(filecontent);
+		pages=loadPage(fs.readFileSync("before_xml/"+item.file,"utf8"));
 	}
 	currentpage=item.page;
-	var t=fetchText(pages[item.page],item.start,item.len);
+	var t=fetchText(pages[item.page],item.offset,item.from.length);
+
 	if (t!==item.from) {
-		error("offset inside tag",item.start);
+		if (!lasterror) lasterror="xml mismatch:"+t;
+		errormerge(item);
+	} else {
+		mergecount++;
+		merge(item);
 	}
 	lastfile=item.file;
 }
-json.map(domerge);
+json.map(trymerge);
+
+savemergefile();
+
+console.log("success merged",mergecount,"unmerged",unmerged.length,"check unmerged.json");
+fs.writeFileSync("unmerged.json",unmerged.join("\n"),"utf8")
--- gen_before_json.js
+++ gen_before_json.js
@@ -22,15 +22,24 @@ var getApproved=function(rows) {
 		var pageid=parseInt(row.doc.pageid);
 		var oldtext=fetchText(fidx,pageid-1,row.doc.start,row.doc.len);
 
-		json.push({f:fidx,seg:pageid-1,file:ljfn,page: segnames[pageid-1],start:row.doc.start, len:row.doc.len, from:oldtext, text:row.doc.payload.text});
+		json.push({f:fidx,p:pageid-1,file:ljfn,page: segnames[pageid-1],offset:row.doc.start, from:oldtext, to:row.doc.payload.text});
 	});
 }
 var doconvert=function(file){
-	var json=require("./"+file);
 	ljfn=file.substr(file.length-17,12).replace(/\\/,"/").replace("/","/lj").replace("-","_")+".xml";
 
-	var rows=json.rows;
+	var rows=require("./"+file).rows;
 	getApproved(rows);
+}
+
+var sortByFilePageOffset=function(){
+	json=json.sort(function(a,b){
+		//assuming offset<65536, total page <4096
+		//reverse order offset for easier merge
+		return (a.f*4096*65536+a.p*65536+ (65536-a.offset)) -(b.f*4096*65536+b.p*65536+ (65536-b.offset));
+	}).map(function(item){
+		return {file:item.file,page:item.page,offset:item.offset,from:item.from,to:item.to};
+	})
 }
 
 kde.open(kdb,function(err,_db){
@@ -40,6 +49,7 @@ kde.open(kdb,function(err,_db){
 		filenames=data[1];
 		var lst=fs.readFileSync("./ketaka_json.lst"||process.argv[3],"utf8").split(/\r?\n/);		
 		lst.map(doconvert);
+		sortByFilePageOffset();
 		fs.writeFileSync("./before_json/"+kdb+".json",JSON.stringify(json,""," "),"utf8");
 	})
 })
